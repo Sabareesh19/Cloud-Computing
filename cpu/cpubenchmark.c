989756fc63ac896c03c21a272e4f4d915d3e42f1
Sabareesh Suresh <33640362+Sabareesh19@users.noreply.github.com>
2018-04-17 20:08:11 -0500

    Update cpubenchmark.c

--- cpu/cpubenchmark.c
+++ cpu/cpubenchmark.c
@@ -7,17 +7,24 @@
 #include <immintrin.h>
 
 
+//strcmp(number_of_arguments[1] - To indicate the Type of precision
+//atoi(number_of_arguments[2] - To indicate the number of threads
+
+
 //FILL ME IN
 
+char **number_of_arguments;
+//char SELECT = 'Q';
+int NUMBER_OF_THREADS = 1;
 long TRILLION_DATA = 1000000000;
-int THREAD_COUNT = 1;
-char FLOPS = 'F';
 
-void* FLOPS_Calculation(int* arg)
+//Calculation of Quarter Precision
+void* Quarter_precision(void* arg)
 {
-
-   //Calculation of Single Precision
-   for(int i = 0;i <= TRILLION_DATA;i++)
+   if(strcmp(number_of_arguments[1],"QP") == 0) 
+   {
+   
+   for(int i = 0;i <= TRILLION_DATA/atoi(number_of_arguments[2]);i++)
      {
        char a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
@@ -30,10 +37,32 @@ void* FLOPS_Calculation(int* arg)
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
      }
+   }
+}
 
-   //Calculation of Half Precision
-   for(int i = 0;i <= TRILLION_DATA;i++)
+
+//Calculation of Half Precision
+void* Half_precision(void* arg)
+{  
+   if(strcmp(number_of_arguments[1],"HP") == 0) 
+   {
+   for(int i = 0;i <= TRILLION_DATA/atoi(number_of_arguments[2]);i++)
      {
        short a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
@@ -46,11 +75,32 @@ void* FLOPS_Calculation(int* arg)
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
     
-     }
+      }
+    }
+}
 
-   //Calculation of SIngle Precision
-   for(int i = 0;i <= TRILLION_DATA;i++)
+//Calculation of Single Precision
+void* single_precision(void* arg)
+{  
+   if(strcmp(number_of_arguments[1],"SP") == 0)  
+   {  
+   for(int i = 0;i <= TRILLION_DATA/atoi(number_of_arguments[2]);i++)
      {
        int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
@@ -63,11 +113,32 @@ void* FLOPS_Calculation(int* arg)
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
-     }
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+      }
+    }
+}
 
-    //Calculation of Double Precision
-   for(int i = 0;i <= TRILLION_DATA;i++)
-     {
+//Calculation of Double Precision
+void* double_precision(void* arg)
+{  
+   if(strcmp(number_of_arguments[1],"DP") == 0)  
+   {    
+   for(int i = 0;i <= TRILLION_DATA/atoi(number_of_arguments[2]);i++)
+      {
        double a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
@@ -79,62 +150,100 @@ void* FLOPS_Calculation(int* arg)
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
        a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
+       a11 = (a1+a2+a3-a4+a5+a6+a7+a8+a9+a10)*a1;
      
-     }
-   pthread_exit(NULL);
+      }
+    }  
 }
 
-//Clock using pthreads
-void clock_operations_per_sec(){
-    pthread_t thread_id[THREAD_COUNT];
-    int thread[THREAD_COUNT];
-    
+//pthread Implementation for the clock execution
+
+void clock_operations_per_sec()
+{
+
+//Calculating time using clock() functions
     double start_time, end_time;
-    start_time = clock();
-    end_time = clock();
-    //gettimeofday(start_time, NULL );
-    
-    if(FLOPS == 'F') {
-        for (int i=0;i<THREAD_COUNT;i++) {
-            thread[i] = pthread_create(&(thread_id[i]),NULL,FLOPS_Calculation,NULL);
+    double flops_ops;
+    start_time = clock();  
+    pthread_t pthread_id[atoi(number_of_arguments[2])];
+    int thread[atoi(number_of_arguments[2])];
+       
+     if(strcmp(number_of_arguments[1],"QP") == 0) 
+    {
+        for (int i=0;i<atoi(number_of_arguments[2]);i++)
+        {
+            thread[i] = pthread_create(&(pthread_id[i]),NULL,Quarter_precision,NULL);
+            
         }
     } 
 
-    for (int i=0;i<THREAD_COUNT;i++) {
-      pthread_join(thread_id[i], NULL);
-    }
-    
-    //gettimeofday(end_time, NULL);
-    double total_time = ((end_time+(end_time/1000000.0))-(start_time+(start_time/1000000.0)));
-    double ops;
-	if (FLOPS == 'F') {
-		ops = (THREAD_COUNT*TRILLION_DATA*26)/total_time;
-	} 
-	printf("CPU, %d threads, speed = %.2lf Giga %s\n",THREAD_COUNT,ops/1000000000,FLOPS == 'F'?"FLOPS/sec":"IOPS/sec");
+     if(strcmp(number_of_arguments[1],"HP") == 0) 
+    {
+        for (int i=0;i<atoi(number_of_arguments[2]);i++)
+        {
+            thread[i] = pthread_create(&(pthread_id[i]),NULL,Half_precision,NULL);
+        }
+    } 
 
+    if(strcmp(number_of_arguments[1],"SP") == 0) 
+    {
+        for (int i=0;i<atoi(number_of_arguments[2]);i++)
+        {
+            thread[i] = pthread_create(&(pthread_id[i]),NULL,single_precision,NULL);
+        }
+    } 
+
+      if(strcmp(number_of_arguments[1],"DP") == 0)  
+    {
+        for (int i=0;i<atoi(number_of_arguments[2]);i++)
+        {
+            thread[i] = pthread_create(&(pthread_id[i]),NULL,double_precision,NULL);
+        }
+    } 
+
+    for (int i=0;i<atoi(number_of_arguments[2]);i++) 
+    {
+            pthread_join(pthread_id[i], NULL);
+      
+    }
+
+    end_time = clock();
+    double total_time = (end_time-start_time)/CLOCKS_PER_SEC; 	
+    
+    flops_ops = (TRILLION_DATA*250*atoi(number_of_arguments[2]))/(total_time); //atoi(number_of_arguments[2] - indicates the number of threads
+    flops_ops = flops_ops/1000000000;
+    printf("*********************************************************\n");
+    printf("Workload      Concurrency      MyCPUBench(GigaOPS) \n");
+    printf("*********************************************************\n");
+    printf( "%s \t \t %d \t \t %lf  \t \t \n",number_of_arguments[1],atoi(number_of_arguments[2]),flops_ops);
+    //printf("The total time consumed by the CPU is %lf \n ", total_time);
+    //printf("The speed of the processor is %lf GigaOPS \n ", flops_ops);
+    //printf("The number of threads executed is %d \n", atoi(number_of_arguments[2]));
     
 }
 
 int main(int argc, char *argv[]){
-   int arg;
-    while((arg=getopt(argc, argv, "ifo:t:")) != -1) {
-        switch(arg) {
-            
-            case 'f':
-            FLOPS='F'; // for FLOPS calculation
-            break;
-            case 'o':
-            TRILLION_DATA=atoi(optarg);
-            case 't':
-            THREAD_COUNT=atoi(optarg);
-            break;
-            default:
-            break;
-        }
-    }
-    calculate_operations_per_sec();
+    
+    number_of_arguments = argv;
+    clock_operations_per_sec();
     return 0;
 }
+
+
 
 
 
